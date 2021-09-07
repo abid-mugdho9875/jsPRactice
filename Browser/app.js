49550e76e24a43ec2c9eb90a5ff99a965c478a0d
abid-mugdho9875 <75389185+abid-mugdho9875@users.noreply.github.com>
2021-09-07 16:22:40 +0600

    end

---
+++ Browser/app.js
@@ -0,0 +1,7 @@
+console.log('hello ')
+console.log('hi')
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+console.log('byby')
+console.log('ok')
---
+++ Browser/settimeout.js
@@ -0,0 +1,7 @@
+console.log('abid hasan')
+console.log('afia khatun munni')
+setTimeout(doSomething = () => {
+    console.log('ok')
+}, 5000)
+console.log('hi')
+console.log('hello')
